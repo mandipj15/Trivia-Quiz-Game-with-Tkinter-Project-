442b430a84a1f046f0c7fa349798dd4af64f8896
Mandip Jadav <166214556+mandipj15@users.noreply.github.com>
2024-06-17 20:34:56 +0530

    Add files via upload

---
+++ question_bank.py
@@ -0,0 +1,197 @@
+question_bank = [
+
+
+{
+        "question": "What is the capital of France?",
+        "choices": ["Paris", "London", "Berlin", "Rome"],
+        "correct_choice": "Paris"
+    },
+    {
+        "question": "Who wrote 'To Kill a Mockingbird'?",
+        "choices": ["J.K. Rowling", "Harper Lee", "Stephen King", "Charles Dickens"],
+        "correct_choice": "Harper Lee"
+    },
+    {
+        "question": "What is the chemical symbol for water?",
+        "choices": ["H2O", "CO2", "NaCl", "O2"],
+        "correct_choice": "H2O"
+    },
+{
+        "question": "What does HTML stand for?",
+        "choices": ["Hyper Text Markup Language", "Hyperlink and Text Markup Language", "Home Tool Markup Language", "Hyper Tool Markup Language"],
+        "correct_choice": "Hyper Text Markup Language"
+    },
+    {
+        "question": "Which programming language is known as the 'language of the web'?",
+        "choices": ["Python", "JavaScript", "Java", "C++"],
+        "correct_choice": "JavaScript"
+    },
+    {
+        "question": "What does CSS stand for?",
+        "choices": ["Creative Style Sheets", "Cascading Style Sheets", "Computer Style Sheets", "Colorful Style Sheets"],
+        "correct_choice": "Cascading Style Sheets"
+    },
+    {
+        "question": "Which city is the capital of Russia?",
+        "choices": ["St.petersburg", "Kazan", "Sochi", "Moscow"],
+        "correct_choice": "Moscow"
+    },
+    {
+        "question": "What year did the Titanic sink in the Atlantic Ocean?",
+        "choices": ["1952", "1913", "1912", "1812"],
+        "correct_choice": "1912"
+    },
+    # {
+    #     "question": "Who wrote the play Romeo and Juliet?",
+    #     "choices": ["William Shakespeare", "Alexander Pushkin", "Victor Hugo"],
+    #     "correct_choice": "William Shakespeare"
+    # },
+    {
+        "question": "Which vitamin is often referred to as the sunshine vitamin? ",
+        "choices": ["vitamin A", "vitamin C", "vitamin D", "vitamin B"],
+        "correct_choice": "vitamin A"
+    },
+    {
+       "question": "Which is the largest island?",
+       "choices": ["New Guinea", "Andaman Nicobar", "Greenland", "Australia"],
+       "correct_choice": "Greenland"
+    },
+    {
+        "question": "Which city is called the city of winds?",
+        "choices": ["Chicago", "Seattle", "Washington", "Veli Novgorod"],
+        "correct_choice": "Chicago"
+    },
+    {
+        "question": "In what year did the French Revolution take place?",
+        "choices": ["1789", "1812", "1793", "1905"],
+        "correct_choice": "1789"
+    },
+    # {
+
+    #  "question": "Who is known as the Father of the Indian Constitution?",
+    #  "choices": ["Mahatma Gandhi", "Jawaharlal Nehru", "Dr.B.R.Ambedkar"],
+    #  "correct_choice": "Dr.B.R.Ambedkar"
+    # },
+    {
+    "question": "The Central Rice Research Station is situated in?",
+     "choices": ["Quilon", "Cuttack", "Bangalore", "Chennai"],
+    "correct_choice": "Cuttack"
+    },
+    {
+        "question": "what is the name of the first Indian women who wins the Man Booker Prize?",
+        "choices": ["Salman Rushdie", "Arundhati Roy", "V.S.Naipaul", "Kiran Desai"],
+        "correct_choice": "Arundhati Roy"
+    },
+    {
+        "question": "Who is the Father of Indian missile technology?",
+        "choices": ["Dr.Homi Bhabha", "Dr.A.P.J.Abdul Kalam", "Dr.U.R.Rao", "Dr.Chidambaram"],
+        "correct_choice": "Dr.A.P.J.Abdul Kalam"
+    },
+    {
+        "question": "In what year did the Great October Revolution take place?",
+        "choices": ["1917", "1912", "1817", "1920"],
+        "correct_choice": "1917"
+    },
+    {
+        "question": "What is the largest lake in the world?",
+        "choices": ["Caspian Sea", "Baikal", "Lake Superior", "Ontario"],
+        "correct_choice": "Baikal"
+    },
+    {
+        "question": "Which planet in the solar system is known as the Red Planet?",
+        "choices": ["Mars", "Jupiter", "Earth", "Venus"],
+        "correct_choice": "Mars"
+    },
+    {
+        "question": "Whay is the capital of Japan?",
+        "choices": ["Beijing", "Tokyo", "Seoul", "Bangkok"],
+        "correct_choice": "Tokyo"
+    },
+    {
+        "question": "What animal is the symbol of Australia?",
+        "choices": ["Kangaroo", "Koala", "Emu", "Crocodile"],
+        "correct_choice": "Kangaroo"
+    },
+    {
+        "question": "In what year was the first international modern Olympiad held?",
+        "choices": ["1896", "1900", "1897", "1955"],
+        "correct_choice": "1896"
+    },
+    {
+        "question": "Which one is the hottest continent?",
+        "choices": ["Africa", "South Asia", "North America", "Australia"],
+        "correct_choice": "Africa"
+    },
+    {
+        "question": "What is the most popular bread in France?",
+        "choices": ["Brioche", "Baguette", "White bread", "Ciabatta"],
+        "correct_choice": "Baguette"
+    },
+    {
+        "question": "What is the official currency of Japan?",
+        "choices": ["Won", "Yuan", "Yen", "Dollars"],
+        "correct_choice": "Yen"
+    },
+    {
+        "question": " Who was the first Indian chief of Army staff of the Indian Army?",
+        "choices": ["Gne.K.M.Cariappa", "Gen.Maharaja Rajiendra Singhji", "Vice.Admiral R.D.Katari", "None of the above"],
+        "correct_choice": "Gen.K.M.Cariappa"
+     },
+    {
+        "question": "Durand CUP is associated whit the game?",
+        "choices": ["Volleyball", "Football", "Cricket", "Hockey"],
+        "correct_choice": "Football"
+    },
+    {
+        "question": "Hockey was introduced in the Asian games?",
+        "choices": ["1966 in Bangkok", "1958 in Tokyo", "1985 in Jakarta", "1970 in Bangkok"],
+        "correct_choice": "1958 in Bangkok"
+    },
+    {
+        "question": "What gas is used to extinguish fires?",
+        "choices": ["Oxygen", "Nitrogen", "Carbon dioxide", "Hydrogen"],
+        "correct_choice": "Hydrogen"
+    },
+    {
+        "question": "What chemical element is designated as Hg",
+        "choices": ["Silver", "Tin", "Copper", "Mercury"],
+        "correct_choice": "Mercury"
+    },
+    {
+        "question": "IN which century the Mona Lisa was painted?",
+        "choices": ["18th century", "15th century", "16th century", "17th century"],
+        "correct_choice": "16th century"
+    },
+    {
+        "question": "Which is the richest countery in the world?",
+        "choices": ["Qatar", "Russia", "The USA", "The UAE"],
+        "correct_choice": "Qatar"
+    },
+    {
+        "question": "Which countery is thr biggest grower coffee?",
+        "choices": ["Spain", "India", "Ethiopia", "Brazil"],
+        "correct_choice": "Brazil"
+    },
+    {
+        "question": "Who wrote Crime and Punishment?",
+        "choices": ["Leo Tolstoy", "Fyodor Dostoevsky", "Anton Chekhov", "Ivan Turgenev"],
+        "correct_choice": "Fyodor Dostoevsky"
+    },
+    {
+        "question": "The Quit India Movement began on?",
+        "choices": ["9th August 1942", "10th August 1942", "11th August 1942", "12th August 1942"],
+        "correct_choice": "9th August 1942"
+    },
+    {
+        "question": "At which place in Bihar was the session of the Indian National Congress of 1922 held?",
+        "choices": ["Bihar", "Patna", "Gaya", "Ramgarh"],
+        "correct_choice": "Gaya"
+    },
+    {
+        "question": "The freedom fighter who died in jail due to hunger strike was?",
+        "choices": ["Bhagat Singh", "Bipin Chandra Pal", "Jatin Das", "Subhash Chandra Bose"],
+        "correct_choice": "Jatin Das"
+    }
+
+
+]
